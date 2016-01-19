f4c8ab991a32397d6f68906253e0e45bda8858b5
Tony Wong <15102792942@163.com>
2016-01-19 18:20:38 +0800

    Update mahalanobis_classifier.m

--- mahalanobis_classifier.m
+++ mahalanobis_classifier.m
@@ -1,3 +1,4 @@
+% example 1.4.1 step2
 function [value]=mahalanobis_classifier(means_vector,covariance_matrix,variables)
 distance1=(means_vector(:,1)-variables)'/covariance_matrix*(means_vector(:,1)-variables);
 distance2=(means_vector(:,2)-variables)'/covariance_matrix*(means_vector(:,2)-variables);
