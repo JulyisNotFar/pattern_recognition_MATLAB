efb6b6ac843010ed86d9355d0b98862b276ca0a6
Tony Wong <15102792942@163.com>
2016-01-20 14:07:29 +0800

    Update mixt_model.m

--- mixt_model.m
+++ mixt_model.m
@@ -1,4 +1,6 @@
 % example 1.5.1
+% This function only works with 2 classes. 
+% For multiple classes, one more loop may be included.
 function [X,y]=mixt_model(m,S,P,N,sed)
 rand('seed',sed);
 X1=mvnrnd(m(:,1)',S(:,:,1),N)';
